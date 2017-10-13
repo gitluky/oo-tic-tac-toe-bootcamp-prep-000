322d933163db4078b321150cf5060a9d4829ec71
gitluky <jluk87@gmail.com>
2017-10-13 15:33:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,50 +1,55 @@
 # Helper Method
 class TicTacToe
 
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+def initialize
+  @@board = [" "," "," "," "," "," "," "," "," "]
+end
+
+
+def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
 end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-def won?(board)
+def won?(@board)
   WIN_COMBINATIONS.detect do |set|
-    board[set[0]] == board[set[1]] &&
-    board[set[1]] == board[set[2]] &&
-    position_taken?(board,set[0])
+    @board[set[0]] == @board[set[1]] &&
+    @board[set[1]] == @board[set[2]] &&
+    position_taken?(set[0])
   end
 end
 
-def full?(board)
-  board.all? {|value| value == "X" || value == "O"}
+def full?(@board)
+  @board.all? {|value| value == "X" || value == "O"}
 end
 
-def draw?(board)
-  full?(board) && !won?(board)
+def draw?(@board)
+  full?(@board) && !won?(@board)
 end
 
-def over?(board)
-  won?(board) || draw?(board)
+def over?(@board)
+  won?(@board) || draw?(@board)
 end
 
-def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
+def winner(@board)
+  if winning_combo = won?(@board)
+    @board[winning_combo.first]
   end
 end
 
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+def display_@board(@board)
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
-def turn_count(board)
+def turn_count(@board)
   count = 0
-  board.each do |location|
+  @board.each do |location|
     if location != " "
       count+=1
     end
@@ -52,44 +57,44 @@ def turn_count(board)
   return count
 end
 
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X": "O"
+def current_player(@board)
+  turn_count(@board) % 2 == 0 ? "X": "O"
 end
 
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player)
-  board[index] = current_player
+def move(@board, index, current_player)
+  @board[index] = current_player
 end
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+def valid_move?(@board, index)
+  index.between?(0,8) && !position_taken?(@board, index)
 end
 
-def turn(board)
+def turn(@board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index, current_player(board))
-    display_board(board)
+  if valid_move?(@board, index)
+    move(@board, index, current_player(@board))
+    display_@board(@board)
   else
-    turn(board)
+    turn(@board)
   end
 end
 
 # Define your play method below
-def play(board)
-  display_board(board)
-  until over?(board)
-    turn(board)
+def play(@board)
+  display_@board(@board)
+  until over?(@board)
+    turn(@board)
   end
 
-  if won?(board)
-    puts ("Congratulations #{winner(board)}!")
-  elsif draw?(board)
+  if won?(@board)
+    puts ("Congratulations #{winner(@board)}!")
+  elsif draw?(@board)
     puts ("Cat's Game!")
   end
 end
