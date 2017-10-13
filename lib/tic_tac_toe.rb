8088e2fbb5890db338c30e26433d6b3d05230f4e
gitluky <jluk87@gmail.com>
2017-10-13 15:38:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,8 +57,8 @@ def turn_count
   return count
 end
 
-def current_player(@board)
-  turn_count(@board) % 2 == 0 ? "X": "O"
+def current_player
+  turn_count % 2 == 0 ? "X": "O"
 end
 
 def input_to_index(user_input)
